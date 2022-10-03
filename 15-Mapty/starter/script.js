fa8ba8dd848a17e96fe1de45a44e904f94e2d0c1
tobennaUmeh <94992053+tobennaUmeh@users.noreply.github.com>
2022-10-03 14:16:01 +0100

    Update script.js

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,11 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(
+  position => {
+    const { latitude, longitude } = position.coords;
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+  },
+  () => alert('Could not get your location')
+);
